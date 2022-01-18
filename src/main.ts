febad676efa6c7bd34ef247257237274b4311819
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-01-18 22:37:36 +0700

    feat(scene): init canvas and context (2d)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 import '@app/style.css';
 
+function resizeCanvas(
+  canvas: HTMLCanvasElement,
+  context: CanvasRenderingContext2D,
+  height: number,
+  width: number
+) : void {
+  canvas.width = width;
+  canvas.height = height;
+
+  context.clearRect(0, 0, width, height);
+}
+
 window.addEventListener('load', () => {
-  // eslint-disable-next-line
-  console.log('hello world.');
+  const canvas: HTMLCanvasElement = document.querySelector('#scene');
+  if (!canvas) return;
+
+  const context: CanvasRenderingContext2D = canvas.getContext('2d');
+  if (!context) return;
+
+  const clientRect: DOMRect = document.documentElement.getBoundingClientRect();
+  const { height, width } = clientRect;
+
+  resizeCanvas(canvas, context, height, width);
 });
